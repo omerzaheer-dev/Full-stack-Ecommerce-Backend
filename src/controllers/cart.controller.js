74047f3c3565f3e983460a541e1b00c28ea8ac76
omerzaheer-dev <umer.zaheer341@gmail.com>
2024-07-27 21:42:07 +0500

    cart controllers

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -26,10 +26,11 @@ const addToCartProduct = asyncHandler(async (req,res) => {
     if (!cart) {
       const newCart = new Cart({ userId, products: [{ productId }] });
       await newCart.save();
+      const kart = await Cart.findOne({ userId }).populate('products.productId').select('-userId');
       return res
         .status(200)
         .json(
-            new ApiResponse(200,newCart,"Product added to Cart")
+            new ApiResponse(200,kart,"Product added to Cart")
         )
     }
     const productExists = cart.products.some(item => item.productId.toString() === productId);
@@ -39,26 +40,28 @@ const addToCartProduct = asyncHandler(async (req,res) => {
             { $pull: { products: { productId: productId } } },
             { new: true }
         );
+        const sentUpdatedCart = await Cart.findOne({ userId }).populate('products.productId').select('-userId');
         return res
         .status(200)
         .json(
-            new ApiResponse(200,updatedCart,"Product removed from cart")
+            new ApiResponse(200,sentUpdatedCart,"Product removed from cart")
         )
     }
 
     cart.products.push({ productId, quantity: 1 });
     await cart.save();
+    const caart = await Cart.findOne({ userId }).populate('products.productId').select('-userId')
 
     return res
         .status(200)
         .json(
-            new ApiResponse(200,cart,"Product added to Cart")
+            new ApiResponse(200,caart,"Product added to Cart")
         )
 
 })
 const productsInCart = asyncHandler(async (req,res) => {
     const userId = req.user
-    const cart = await Cart.findOne({ userId });
+    const cart = await Cart.findOne({ userId }).populate('products.productId').select('-userId');;
     if(!cart){
         const Cart = await Cart.create({
             userId,
@@ -76,7 +79,80 @@ const productsInCart = asyncHandler(async (req,res) => {
             new ApiResponse(200,cart,"Cart Returned")
         )
 })
+const viewCartProducts = asyncHandler(async (req , res) => {
+    const userId = req?.user?._id;
+    if(!userId){
+        return res
+        .status(402)
+        .json(
+                new ApiError(401,"User is not present")
+            ) 
+    }
+    const cart = await Cart.findOne({
+        userId
+    }).populate('products.productId').select('-userId');
+    if(!cart){
+        return res
+        .status(402)
+        .json(
+                new ApiError(401,"Cart is not defined")
+            )
+    }
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200,cart,"Product removed from cart")
+        )
+})
+const updateAddToCartProducts = asyncHandler(async (req , res) => {
+    const userId = req?.user?._id;
+    const {_id,qty,type} = req?.body;
+    if(type==='dec' && qty===1){
+        return res
+        .status(402)
+        .json(
+                new ApiError(401,"cannot decrement")
+            )
+    }
+    if(!userId || !_id || !qty){
+        return res
+        .status(402)
+        .json(
+                new ApiError(401,"All fiels are required")
+            )
+    }
+    const cart = await Cart.findOne({ userId });
+    if (!cart) {
+        return res
+        .status(402)
+        .json(
+                new ApiError(401,"Cart not Found")
+            )
+    }
+    const product = cart.products.find(p => p.productId.toString() === _id);
+    if (!product) {
+        return res
+        .status(402)
+        .json(
+                new ApiError(401,"product not found")
+            )
+    }
+    if(type==='dec'){
+        product.quantity = qty-1;
+    }
+    if(type==='inc'){
+        product.quantity = qty+1;
+    }
+    await cart.save();
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200,{},"Cart updated successfully")
+        )
+})
 export {
     addToCartProduct,
-    productsInCart
+    productsInCart,
+    viewCartProducts,
+    updateAddToCartProducts
 }
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { jwtVerify } from "../middlewares/auth.middleware.js"
-import { addToCartProduct , productsInCart } from "../controllers/cart.controller.js"
+import { addToCartProduct , productsInCart , viewCartProducts , updateAddToCartProducts } from "../controllers/cart.controller.js"
 
 const router = Router()
 router.route("/add-toCart").post(jwtVerify,addToCartProduct)
 router.route("/get-cart-products").get(jwtVerify,productsInCart)
+router.route("/view-cart-products").get(jwtVerify,viewCartProducts)
+router.route("/update-addToCart").patch(jwtVerify,updateAddToCartProducts)
 export default router
